e943a755d422588438a7340369d039bbf360458f
serraguti <pacoserranox@gmail.com>
2025-02-03 13:20:49 +0100

    Add project files.

---
+++ MvcCoreAdoNet/Controllers/HospitalesController.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MvcCoreAdoNet.Models;
+using MvcCoreAdoNet.Repositories;
+
+namespace MvcCoreAdoNet.Controllers
+{
+    public class HospitalesController : Controller
+    {
+        private RepositoryHospital repo;
+
+        public HospitalesController()
+        {
+            this.repo = new RepositoryHospital();
+        }
+
+        public IActionResult Index()
+        {
+            List<Hospital> hospitales = this.repo.GetHospitales();
+            return View(hospitales);
+        }
+
+        public IActionResult Details(int id)
+        {
+            Hospital hospital = this.repo.FindHospital(id);
+            return View(hospital);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Hospital hospital)
+        {
+            this.repo.CreateHospital(hospital.IdHospital, hospital.Nombre
+                , hospital.Direccion, hospital.Telefono, hospital.Camas);
+            ViewData["MENSAJE"] = "Hospital insertado";
+            return View();
+        }
+
+        public IActionResult Update(int id)
+        {
+            Hospital hospital = this.repo.FindHospital(id);
+            return View(hospital);
+        }
+
+        [HttpPost]
+        public IActionResult Update(Hospital hospital)
+        {
+            this.repo.UpdateHospital(hospital.IdHospital
+                , hospital.Nombre, hospital.Direccion
+                , hospital.Telefono, hospital.Camas);
+            ViewData["MENSAJE"] = "Hospital modificado";
+            return View(hospital);
+        }
+    }
+}
---
+++ MvcCoreAdoNet/Repositories/RepositoryHospital.cs
@@ -0,0 +1,111 @@
+﻿using Microsoft.Data.SqlClient;
+using MvcCoreAdoNet.Models;
+
+namespace MvcCoreAdoNet.Repositories
+{
+    public class RepositoryHospital
+    {
+        SqlConnection cn;
+        SqlCommand com;
+        SqlDataReader reader;
+
+        public RepositoryHospital()
+        {
+            string connectionString = @"Data Source=LOCALHOST\DESARROLLO;Initial Catalog=HOSPITAL;Persist Security Info=True;User ID=SA;Encrypt=True;Trust Server Certificate=True";
+            this.cn = new SqlConnection(connectionString);
+            this.com = new SqlCommand();
+            this.com.Connection = this.cn;
+        }
+
+        public List<Hospital> GetHospitales()
+        {
+            string sql = "select * from HOSPITAL";
+            this.com.CommandType = System.Data.CommandType.Text;
+            this.com.CommandText = sql;
+            this.cn.Open();
+            this.reader = this.com.ExecuteReader();
+            List<Hospital> hospitales = new List<Hospital>();
+            while (this.reader.Read())
+            {
+                Hospital hospital = new Hospital();
+                hospital.IdHospital =
+                    int.Parse(this.reader["HOSPITAL_COD"].ToString());
+                hospital.Nombre =
+                    this.reader["NOMBRE"].ToString();
+                hospital.Direccion =
+                    this.reader["DIRECCION"].ToString();
+                hospital.Telefono =
+                    this.reader["TELEFONO"].ToString();
+                hospital.Camas =
+                    int.Parse(this.reader["NUM_CAMA"].ToString());
+                hospitales.Add(hospital);
+            }
+            this.reader.Close();
+            this.cn.Close();
+            return hospitales;
+        }
+
+        public Hospital FindHospital(int idhospital)
+        {
+            string sql = 
+                "select * from HOSPITAL where HOSPITAL_COD=@idhospital";
+            this.com.Parameters.AddWithValue("@idhospital", idhospital);
+            this.com.CommandType = System.Data.CommandType.Text;
+            this.com.CommandText = sql;
+            this.cn.Open();
+            this.reader = this.com.ExecuteReader();
+            Hospital hospital = new Hospital();
+            this.reader.Read();
+            hospital.IdHospital =
+                int.Parse(this.reader["HOSPITAL_COD"].ToString());
+            hospital.Nombre = this.reader["NOMBRE"].ToString();
+            hospital.Direccion = this.reader["DIRECCION"].ToString();
+            hospital.Telefono = this.reader["TELEFONO"].ToString();
+            hospital.Camas =
+                int.Parse(this.reader["NUM_CAMA"].ToString());
+            this.reader.Close();
+            this.cn.Close();
+            this.com.Parameters.Clear();
+            return hospital;
+        }
+
+        public void CreateHospital
+            (int idHospital, string nombre, string direccion
+            , string telefono, int camas)
+        {
+            string sql = "insert into HOSPITAL values (@idhospital, @nombre "
+                + ", @direccion, @telefono, @camas)";
+            this.com.Parameters.AddWithValue("@idhospital", idHospital);
+            this.com.Parameters.AddWithValue("@nombre", nombre);
+            this.com.Parameters.AddWithValue("@direccion", direccion);
+            this.com.Parameters.AddWithValue("@telefono", telefono);
+            this.com.Parameters.AddWithValue("@camas", camas);
+            this.com.CommandType = System.Data.CommandType.Text;
+            this.com.CommandText = sql;
+            this.cn.Open();
+            this.com.ExecuteNonQuery();
+            this.cn.Close();
+            this.com.Parameters.Clear();
+        }
+
+        public void UpdateHospital(int idHospital, 
+            string nombre, string direccion, string telefono, int camas)
+        {
+            string sql = "update HOSPITAL set NOMBRE=@nombre "
+                + ", DIRECCION=@direccion, TELEFONO=@telefono "
+                + ", NUM_CAMA=@camas "
+                + " where HOSPITAL_COD=@idhospital";
+            this.com.Parameters.AddWithValue("@nombre", nombre);
+            this.com.Parameters.AddWithValue("@direccion", direccion);
+            this.com.Parameters.AddWithValue("@telefono", telefono);
+            this.com.Parameters.AddWithValue("@camas", camas);
+            this.com.Parameters.AddWithValue("@idhospital", idHospital);
+            this.com.CommandType = System.Data.CommandType.Text;
+            this.com.CommandText = sql;
+            this.cn.Open();
+            this.com.ExecuteNonQuery();
+            this.cn.Close();
+            this.com.Parameters.Clear();
+        }
+    }
+}
