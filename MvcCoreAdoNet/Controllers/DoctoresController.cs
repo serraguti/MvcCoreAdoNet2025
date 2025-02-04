6817d5c7a4fc2a11750081620b138302e81326bb
serraguti <pacoserranox@gmail.com>
2025-02-04 09:49:46 +0100

    sdadsad

---
+++ MvcCoreAdoNet/Controllers/DoctoresController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MvcCoreAdoNet.Models;
+using MvcCoreAdoNet.Repositories;
+
+namespace MvcCoreAdoNet.Controllers
+{
+    public class DoctoresController : Controller
+    {
+        RepositoryDoctor repo;
+
+        public DoctoresController()
+        {
+            this.repo = new RepositoryDoctor();
+        }
+
+        public async Task<IActionResult> DoctoresEspecialidad()
+        {
+            List<Doctor> doctores = await this.repo.GetDoctoresAsync();
+            return View(doctores);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> 
+            DoctoresEspecialidad(string especialidad)
+        {
+            List<Doctor> doctores =
+                await this.repo.GetDoctoresEspecialidadAsync(especialidad);
+            return View(doctores);
+        }
+    }
+}
---
+++ MvcCoreAdoNet/Repositories/RepositoryDoctor.cs
@@ -0,0 +1,74 @@
+﻿using Microsoft.Data.SqlClient;
+using MvcCoreAdoNet.Models;
+
+namespace MvcCoreAdoNet.Repositories
+{
+    public class RepositoryDoctor
+    {
+        SqlConnection cn;
+        SqlCommand com;
+        SqlDataReader reader;
+
+        public RepositoryDoctor()
+        {
+            string connectionString = @"Data Source=LOCALHOST\DESARROLLO;Initial Catalog=HOSPITAL;Persist Security Info=True;User ID=SA;Encrypt=True;Trust Server Certificate=True";
+            this.cn = new SqlConnection(connectionString);
+            this.com = new SqlCommand();
+            this.com.Connection = this.cn;
+        }
+
+        public async Task<List<Doctor>> GetDoctoresAsync()
+        {
+            string sql = "select * from DOCTOR";
+            this.com.CommandType = System.Data.CommandType.Text;
+            this.com.CommandText = sql;
+            await this.cn.OpenAsync();
+            this.reader = await this.com.ExecuteReaderAsync();
+            List<Doctor> doctores = new List<Doctor>();
+            while (await this.reader.ReadAsync()){
+                Doctor doc = new Doctor
+                {
+                    IdDoctor = int.Parse(this.reader["DOCTOR_NO"].ToString()),
+                    Apellido = this.reader["APELLIDO"].ToString(),
+                    Especialidad = this.reader["ESPECIALIDAD"].ToString(),
+                    Salario = int.Parse(this.reader["SALARIO"].ToString()),
+                    IdHospital =
+                    int.Parse(this.reader["HOSPITAL_COD"].ToString())
+                };
+                doctores.Add(doc);
+            }
+            await this.reader.CloseAsync();
+            await this.cn.CloseAsync();
+            return doctores;
+        }
+
+        public async Task<List<Doctor>> 
+            GetDoctoresEspecialidadAsync(string especialidad)
+        {
+            string sql = 
+                "select * from DOCTOR where ESPECIALIDAD=@especialidad";
+            this.com.Parameters.AddWithValue("@especialidad", especialidad);
+            this.com.CommandType = System.Data.CommandType.Text;
+            this.com.CommandText = sql;
+            await this.cn.OpenAsync();
+            this.reader = await this.com.ExecuteReaderAsync();
+            List<Doctor> doctores = new List<Doctor>();
+            while (await this.reader.ReadAsync())
+            {
+                Doctor doc = new Doctor
+                {
+                    IdDoctor = int.Parse(this.reader["DOCTOR_NO"].ToString()),
+                    Apellido = this.reader["APELLIDO"].ToString(),
+                    Especialidad = this.reader["ESPECIALIDAD"].ToString(),
+                    Salario = int.Parse(this.reader["SALARIO"].ToString()),
+                    IdHospital = int.Parse(this.reader["HOSPITAL_COD"].ToString())
+                };
+                doctores.Add(doc);
+            }
+            await this.reader.CloseAsync();
+            await this.cn.CloseAsync();
+            this.com.Parameters.Clear();
+            return doctores;
+        }
+    }
+}
